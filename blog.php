1202db3e343dd3ae1976ddbda757d72458223995
johnerving <146698414+johnerving@users.noreply.github.com>
2024-02-05 20:20:24 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Blog Section</title>
+    <link rel="stylesheet" type="text/css" href="blog.css"/>
+    <link rel="shortcut icon" href="images/fav-icon.svg"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section id="blog">
+            <div class="blog-heading">
+              <span>Blog Site</span>
+              <h3>HOBBIES</h3>
+            </div>
+      
+      
+            <div class="blog-container">
+      
+              <div class="blog-box">
+      
+
+                <div class="blog-img">
+                  <img alt="blog" src="picture/solo.jfif">
+                </div>
+
+                <div class="blog-text">
+                  <span>ANIME</span>
+                  <a href="#" class="blog-title">solo leveling</a>
+                  <p>“Solo Leveling” is set in South Korea, in a world where portals to dungeons filled with different monsters appeared randomly. People with powers are known as “hunters” and fight in dungeons.</p>
+                  <a href="faker.html">See More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+   
+              <div class="blog-box">
+      
+                <div class="blog-img">
+                  <img alt="blog" src="picture/volleyball1.jfif">
+                </div>
+                <div class="blog-text">
+                  <span>SPORTS</span>
+                  <a href="#" class="blog-title">volleyball</a>
+                  <p>A game for two teams, usually of six players, in which a large ball is hit by hand over a high net, the aim being to score points by making the ball reach the ground on the opponent's side of the court.</p>
+                  <a href="faker.html">See More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+              
+              <div class="blog-box">
+      
+                
+                <div class="blog-img">
+                  <img alt="blog" src="picture/ml.jfif">
+                </div>
+                
+                <div class="blog-text">
+                  <span>MOBILE GAMES</span>
+                  <a href="#" class="blog-title">ML</a>
+                  <p> Mobile Legends: Bang Bang (MLBB) is a mobile multiplayer online battle arena (MOBA) game developed and published by Moonton, a subsidiary of ByteDance. Released in 2016, the game grew in popularity; most prominently in Southeast Asia.</p>
+                  <a href="faker.html">Read More</a>
+                </div>
+      
+              </div>
+      
+            </div>
+      
+          </section>
+          
+    </body>
+    </html>
